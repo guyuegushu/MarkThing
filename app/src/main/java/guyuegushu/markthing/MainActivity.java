04647c55fcab90f881c1026bdd866406d02d784a
hushu <hushu@126.com>
2016-12-22 16:37:14 +0800

    全部添加到一个activity中显示

--- app/src/main/java/guyuegushu/markthing/MainActivity.java
+++ app/src/main/java/guyuegushu/markthing/MainActivity.java
@@ -1,12 +1,17 @@
 package guyuegushu.markthing;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    private  MonthListFragment mlF = new MonthListFragment();
+    private MarkFragment mF = new MarkFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,25 +21,35 @@ public class MainActivity extends Activity {
     }
 
     private void singleItem() {
-
         TextView month = (TextView) findViewById(R.id.main_time);
         TimeYMDH timeYMDH = new TimeYMDH();
         month.setText(timeYMDH.getYMD());
     }
-//    btn_mark_today  record_month
 
     public void btn_mark_today(View view){
-
-        Intent intent = new Intent(this, FragmentDisActivity.class);
-        intent.putExtra("frag", "Mark");
-        startActivity(intent);
+        disMark();
     }
 
     public void record_month(View view){
-
-        Intent intent = new Intent(this, FragmentDisActivity.class);
-        intent.putExtra("frag", "Month");
-        startActivity(intent);
-
+        disMonth();
     }
+
+    private void disMark(){
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction
+                .remove(mlF)
+                .replace(R.id.mark_main, mF)
+                .commit();
+    }
+
+    private void disMonth(){
+
+        getFragmentManager().beginTransaction()
+                .remove(mF)
+                .replace(R.id.mark_main, mlF)
+                .commit();
+    }
+
 }
